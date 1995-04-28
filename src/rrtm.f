0451cf13002214117058ff52dce00550a06eb195
patbrown <patbrown>
1995-04-28 11:24:05 +0000

    This commit was generated by cvs2svn to compensate for changes in r16, which included commits to RCS files with non-trunk default branches.

--- src/rrtm.f
+++ src/rrtm.f
@@ -1,3 +1,7 @@
+C     path:      %P%
+C     revision:  $Revision$
+C     created:   $Date$  
+C     presently: %H%  %T%
 ****************************************************************************
 *                                                                          *
 *                               RRTM                                       *
@@ -44,14 +48,14 @@ C     c) writes out the upward, downward, and net flux for each
 C        level and the heating rate for each layer
 C     Keep in mind that AER_RRTM is still a work in progress.
 
-      PARAMETER (MXLAY=200)
+      PARAMETER (MXLAY=203)
       PARAMETER (MG = 16)
       PARAMETER (NBANDS = 16)
 
       COMMON /FEATURES/  NG(NBANDS),NSPA(MG),NSPB(MG)
       COMMON /CONTROL/   NUMANGS
       COMMON /PROFILE/   NLAYERS,PAVEL(MXLAY),TAVEL(MXLAY),
-     &                   PZ(0:MXLAY),TZ(0:MXLAY)
+     &                   PZ(0:MXLAY),TZ(0:MXLAY),TSFC
       COMMON /OUTPUT/    TOTUFLUX(0:MXLAY), TOTDFLUX(0:MXLAY),
      &                   FNET(0:MXLAY), HTR(0:MXLAY)
       COMMON /CONSTANTS/ PI,FLUXFAC,HEATFAC
@@ -74,6 +78,7 @@ C        =  (9.8066)(3600)(1e-5)/(1.004)
       IANGFLAG = 1
       NUMANGS = 1
       IWR = 10
+         OPEN (IWR,FILE='OUTPUT_RRTM',FORM='FORMATTED')
 
       IF (ISETUP .EQ. 1) CALL INIT
 
@@ -91,8 +96,6 @@ C               and is equal to the number of output values of
 C               heating rate.
 C     Only NLAYERS is needed by RT.
 
-C     Multiple atmospheres not yet implemented. 
-      DO 4000 IATMOS = 1, NUMATMOS
 
 C ***    Input layer information from INPUT_RRTM
          CALL READPROF
@@ -107,7 +110,6 @@ C ***    Call the radiative transfer routine.
          CALL RT 
 
 C ***    Process output for this atmosphere.
-         OPEN (IWR,FILE='OUTPUT_RRTM',FORM='FORMATTED')
          WRITE(IWR,9900)
          WRITE(IWR,9901)
 C
@@ -118,7 +120,6 @@ C
 
          CLOSE(IWR)
 
- 4000 CONTINUE
 
  9900 FORMAT(1X,'LEVEL    PRESSURE   UPWARD FLUX   DOWNWARD FLUX    NET    
      &FLUX        HEATING RATE')
@@ -134,52 +135,112 @@ C************************  SUBROUTINE READPROF  *****************************C
                                                                          
       IMPLICIT DOUBLE PRECISION (V)                                      
                                                                          
-      PARAMETER (MXLAY=200)
-      DIMENSION WBRODL(MXLAY), WKL(35,MXLAY), ALTZ(0:MXLAY)
+      PARAMETER (MXLAY=203)
+      DIMENSION ALTZ(0:MXLAY)
+
+
+      COMMON /IFIL/ IRD,IPR,IPU,DUM(15)
 
       COMMON /PROFILE/ NLAYERS,PAVEL(MXLAY),TAVEL(MXLAY),
-     &               PZ(0:MXLAY),TZ(0:MXLAY)
-      COMMON /SPECIES/COLDRY(MXLAY),WATER(MXLAY),CO2(MXLAY),OZ(MXLAY)
- 
-   10 FORMAT (////)
-   11 FORMAT (E10.3,E10.3)
-   12 FORMAT (1X,I1,I3,I5)                                     
-   13 FORMAT (G15.7,G10.4,G10.4,A3,I2,1X,2(G7.2,G8.3,G7.2))
-   14 FORMAT (G15.7,G10.4,G10.4,A3,I2,23X,(G7.2,G8.3,G7.2))
-   15 FORMAT (8G15.7)
-                                                                         
+     &                 PZ(0:MXLAY),TZ(0:MXLAY),TSFC
+      COMMON /SPECIES/ COLDRY(MXLAY),WKL(35,MXLAY),WBRODL(MXLAY),NMOL
+      COMMON /MOLAMNT/DRYAIR(MXLAY),WH2O(MXLAY),WCO2(MXLAY),WO3(MXLAY)
+
+      CHARACTER*80 FORM1(0:1),FORM2(0:1),FORM3(0:1)
+      CHARACTER*1 CTEST, CDOLLAR
+
+      DATA CDOLLAR /'$'/
+
+      FORM1(0) = '(3F10.4,A3,I2,1X,2(F7.2,F8.3,F7.2))'
+      FORM2(0) = '(3F10.4,A3,I2,23X,(F7.2,F8.3,F7.2))'
+      FORM3(0) = '(8E10.3)'
+      FORM1(1) = '(G15.7,G10.4,G10.4,A3,I2,1X,2(G7.2,G8.3,G7.2))'
+      FORM2(1) = '(G15.7,G10.4,G10.4,A3,I2,23X,(G7.2,G8.3,G7.2))'
+      FORM3(1) = '(8G15.7)'
+
       IRD = 9
-      OPEN (IRD,FILE='INPUT_RRTM',FORM='FORMATTED')
-      READ (IRD,10)
-      READ (IRD,11) V1,V2
-      READ (IRD,12) IFORM,NLAYERS,NMOL
+      OPEN (IRD,FILE='TAPE5',FORM='FORMATTED')
 
-      IF (NMOL.EQ.0) NMOL = 7                                          
-                                                                         
-      READ (IRD,13) PAVEL(1),TAVEL(1),SECNTK,CINP,IPTHAK,
-     &              ALTZ(0),PZ(0),TZ(0),ALTZ(1),PZ(1),TZ(1)
-      READ (IRD,15) (WKL(M,1),M=1,7), WBRODL(1)
-      WATER(1) = WKL(1,1)
-      CO2(1) = WKL(2,1)
-      OZ(1) = WKL(3,1)
-      COLDRY(1) = WBRODL(1)/(1.-CO2(1)-WKL(3,1)-WKL(4,1)-WKL(5,1)
-     &     -WKL(6,1)-WKL(7,1))
-      IF(NMOL .GT. 7) READ (IRD,15) (WKL(M,1),M=8,NMOL)
+ 1000 CONTINUE
+      READ (IRD,9010,END=8800) CTEST
+      IF (CTEST .NE. CDOLLAR) GO TO 1000
 
-      DO 100 L = 2, NLAYERS                                                 
-           READ (IRD,14) PAVEL(L),TAVEL(L),SECNTK,CINP,IPTHRK,
-     &                   ALTZ(L),PZ(L),TZ(L)
-           READ (IRD,15) (WKL(M,L),M=1,7), WBRODL(L)
-           WATER(L) = WKL(1,L)
-           CO2(L) = WKL(2,L)
-           OZ(L) = WKL(3,L)
-           COLDRY(L) = WBRODL(L)/(1.-CO2(L)-WKL(3,L)-WKL(4,L)
-     &          -WKL(5,L)-WKL(6,L)-WKL(7,L))
-C           WRITE(*,*)L,CO2(L),COLCO2(L),COLDRY
-           IF(NMOL .GT. 7) READ (IRD,15) (WKL(M,L),M=8,NMOL)
-  100 CONTINUE                                                            
+      READ(IRD,9011) IATM
+      READ (IRD,9012) TSFC
+
+      IF (IATM .EQ. 0) THEN
+         READ (IRD,9013) IFORM,NLAYERS,NMOL
+         IF (NMOL.EQ.0) NMOL = 7                                    
+         READ (IRD,FORM1(IFORM)) PAVEL(1),TAVEL(1),SECNTK,CINP,
+     &        IPTHAK,ALTZ(0),PZ(0),TZ(0),ALTZ(1),PZ(1),TZ(1)
+         READ (IRD,FORM3(IFORM)) (WKL(M,1),M=1,7), WBRODL(1)
+         IF(NMOL .GT. 7) READ (IRD,FORM3(IFORM)) (WKL(M,1),M=8,NMOL)
+
+         SUMMOL = 0.0
+         DO 2000 IMOL = 2, NMOL
+            SUMMOL = SUMMOL + WKL(IMOL,1)
+ 2000    CONTINUE
+
+C        Test for mixing ratio input.
+         IMIX = 0
+         IF (WKL(2,1) .LE. 1.0) IMIX = 1
+         IF (IMIX .EQ. 1) THEN
+            DRAIR = WBRODL(1) / (1. - SUMMOL)
+            DO 2500 IMOL = 1, NMOL
+               WKL(IMOL,1) = DRAIR * WKL(IMOL,1)
+ 2500       CONTINUE
+         ENDIF
+
+      DO 4000 L = 2, NLAYERS
+           READ (IRD,FORM2(IFORM)) PAVEL(L),TAVEL(L),SECNTK,CINP,
+     &        IPTHRK,ALTZ(L),PZ(L),TZ(L)
+           READ (IRD,FORM3(IFORM)) (WKL(M,L),M=1,7), WBRODL(L)
+           IF(NMOL .GT. 7) READ (IRD,FORM3(IFORM)) (WKL(M,L),M=8,NMOL)
+
+           SUMMOL = 0.0
+           DO 3000 IMOL = 2, NMOL
+              SUMMOL = SUMMOL + WKL(IMOL,L)
+ 3000      CONTINUE
+           
+           IF (IMIX .EQ. 1) THEN
+              DRAIR = WBRODL(L) / (1. - SUMMOL)
+              DO 3500 IMOL = 1, NMOL
+                 WKL(IMOL,L) = DRAIR * WKL(IMOL,L)
+ 3500         CONTINUE
+           ENDIF
+ 4000   CONTINUE                                                            
+
+      ELSE
+         IPU = 7
+         IPR = 66
+         OPEN(UNIT=IPR,FILE='TAPE6',STATUS='UNKNOWN')
+         CALL RRTATM
+      ENDIF
 
       CLOSE(IRD)
+      GO TO 9000
+
+ 8800 CONTINUE
+      STOP ' INVALID INPUT_RRTM '
+
+ 9000 CONTINUE
+c
+c
+      DO 100 L = 1, NLAYERS                                                 
+         COLDRY(L) = WBRODL(L)
+         DO 90 M = 2,NMOL
+            COLDRY(L) = COLDRY(L) + WKL(M,L)
+ 90      CONTINUE
+         DRYAIR(L) = COLDRY(L)
+         WH2O(L) = WKL(1,L)/COLDRY(L)
+         WCO2(L) = WKL(2,L)/COLDRY(L)
+         WO3(L)  = WKL(3,L)/COLDRY(L)
+ 100  CONTINUE                                                            
+c
+ 9010 FORMAT (A1)
+ 9011 FORMAT (49X,I1)
+ 9012 FORMAT (E10.3)
+ 9013 FORMAT (1X,I1,I3,I5)                                     
 
       RETURN
       END 
