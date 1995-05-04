b5ee10789f955351708e9f2abcbf6931f5392646
no_author <no_author@no_author>
1995-05-04 11:48:01 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.1.1'.

---
+++ src/k_g.f
@@ -0,0 +1,11 @@
+C     path:      %P%
+C     revision:  $Revision$
+C     created:   $Date$  
+C     presently: %H%  %T%
+
+      INCLUDE 'k_gB1.dat'
+      INCLUDE 'k_gB2.dat'
+      INCLUDE 'k_gB3.dat'
+      INCLUDE 'k_gB4.dat'
+      INCLUDE 'k_gB5.dat'
+      INCLUDE 'k_gB6.dat'
