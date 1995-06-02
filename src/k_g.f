7434322b15ab0eef011522ff1a03e7f1602b1aac
mlawer <mlawer>
1995-06-02 16:55:26 +0000

    added bands 7-16

--- src/k_g.f
+++ src/k_g.f
@@ -1,15 +1,17 @@
-C     path:      %P%
-C     revision:  $Revision$
-C     created:   $Date$  
-C     presently: %H%  %T%
-
-*******************************************************************************
-
-      INCLUDE 'k_gB1.f'
-      INCLUDE 'k_gB2.f'
-      INCLUDE 'k_gB3.f'
-      INCLUDE 'k_gB4.f'
-      INCLUDE 'k_gB5.f'
-      INCLUDE 'k_gB6.f'
-
-*******************************************************************************
+      INCLUDE 'k_gB1.dat'
+      INCLUDE 'k_gB2.dat'
+      INCLUDE 'k_gB3.dat'
+      INCLUDE 'k_gB4.dat'
+      INCLUDE 'k_gB5.dat'
+      INCLUDE 'k_gB6.dat'
+      INCLUDE 'k_gB7.dat'
+      INCLUDE 'k_gB8.dat'
+      INCLUDE 'k_gB9.dat'
+      INCLUDE 'k_gB10.dat'
+      INCLUDE 'k_gB11.dat'
+      INCLUDE 'k_gB12.dat'
+      INCLUDE 'k_gB13.dat'
+      INCLUDE 'k_gB14.dat'
+      INCLUDE 'k_gB15.dat'
+      INCLUDE 'k_gB16.dat'
+      
