6fe7024b195a173a39e576b8685a3824f2c99896
mlawer <mlawer>
1996-07-12 17:27:31 +0000

    included 'block data', 'end' statements

--- src/k_g.f
+++ src/k_g.f
@@ -4,21 +4,86 @@ C     created:   $Date$
 C     presently: %H%  %T%
 ****************************************************************************
 
+      BLOCK DATA KGB1
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB01.f'
+      END
+
+      BLOCK DATA KGB2
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB02.f'
+      END
+
+      BLOCK DATA KGB3
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB03.f'
+      END
+
+      BLOCK DATA KGB4
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB04.f'
+      END
+
+      BLOCK DATA KGB5
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB05.f'
+      END
+
+      BLOCK DATA KGB6
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB06.f'
+      END
+
+      BLOCK DATA KGB7
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB07.f'
+      END
+
+      BLOCK DATA KGB8
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB08.f'
+      END
+
+      BLOCK DATA KGB9
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB09.f'
+      END
+
+      BLOCK DATA KGB10
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB10.f'
+      END
+
+      BLOCK DATA KGB11
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB11.f'
+      END
+
+      BLOCK DATA KGB12
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB12.f'
+      END
+
+      BLOCK DATA KGB13
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB13.f'
+      END
+
+      BLOCK DATA KGB14
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB14.f'
+      END
+
+      BLOCK DATA KGB15
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB15.f'
+      END
+
+      BLOCK DATA KGB16
+      PARAMETER (NBANDS = 16)
       INCLUDE 'k_gB16.f'
+      END
+
       
 ****************************************************************************
+
