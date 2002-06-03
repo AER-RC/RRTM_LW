ab1bb6a80cf1fdf7d962877041c99be3a711a9be
jdelamer <jdelamer>
2002-06-03 20:48:35 +0000

    Changed rapid, default RT calculation from Lacis angles (see RRTM 1997 paper) to a one-angle angle calculation (standard quadrature).

--- src/rtr.f
+++ src/rtr.f
@@ -1,24 +1,26 @@
-C     path:      %P%
+C     path:      $Source$
+C     author:    $Author$
 C     revision:  $Revision$
-C     created:   $Date$  
-C     presently: %H%  %T%
+C     created:   $Date$
       SUBROUTINE RTR
 
 C *** This program calculates the upward fluxes, downward fluxes,
 C     and heating rates for an arbitrary atmosphere.  The input to
 C     this program is the atmospheric profile and all Planck function
-C     information.  First-order "numerical" quadrature is used for the 
-C     angle integration, i.e. only one exponential is computed per layer
-C     per g-value per band.
+C     information.  Only one angle is used from standard 
+C     Gaussian quadrature.
 
       PARAMETER (MG=16)
       PARAMETER (MXLAY=203)
       PARAMETER (MXANG = 4)
       PARAMETER (NBANDS = 16)
+      PARAMETER (NTBL = 10000,TBLINT=10000.0)
 
       IMPLICIT DOUBLE PRECISION (V)                                     
 
-      COMMON /CONSTANTS/ PI,FLUXFAC,HEATFAC
+      COMMON /CONSTANTS/ FLUXFAC,HEATFAC
+      COMMON /CONSTS/ PI,PLANCK,BOLTZ,CLIGHT,AVOGAD,ALOSMT,GASCON,
+     *                RADCN1,RADCN2 
       COMMON /FEATURES/  NG(NBANDS),NSPA(NBANDS),NSPB(NBANDS)
       COMMON /BANDS/     WAVENUM1(NBANDS),WAVENUM2(NBANDS),
      &                   DELWAVE(NBANDS)
@@ -32,37 +34,30 @@ C     per g-value per band.
       COMMON /TAUGCOM/   TAUG(MXLAY,MG)
       COMMON /OUTPUT/    TOTUFLUX(0:MXLAY), TOTDFLUX(0:MXLAY),
      &                   FNET(0:MXLAY), HTR(0:MXLAY)
+      COMMON /RTTBL/     BPADE,
+     &                   TAUTBL(0:NTBL),TRANS(0:NTBL),TF(0:NTBL)
       COMMON /HVERSN/    HVRRTM,HVRREG,HVRRTR,HVRATM,HVRSET,HVRTAU,
-     *                   HVDUM1(4),HVRUTL,HVREXT
+     *                   HVRRGC,HVRRTC,HVRCLD,HVRUTL,HVREXT,
+     *                   HVRRTX,HVRRGX
 
       CHARACTER*15 HVRRTM,HVRREG,HVRRTR,HVRATM,HVRSET,HVRTAU,
-     *            HVDUM1,HVRUTL,HVREXT
+     *            HVRRGC,HVRRTC,HVRCLD,HVRUTL,HVREXT,
+     *            HVRRTX,HVRRGX
                                        
-      DIMENSION BBU1(MXLAY),BBU2(MXLAY),BBU3(MXLAY)
-      DIMENSION ATRANS1(MXLAY),ATRANS2(MXLAY),ATRANS3(MXLAY)
+      DIMENSION BBU1(MXLAY)
+      DIMENSION ATRANS1(MXLAY)
       DIMENSION UFLUX(0:MXLAY),DFLUX(0:MXLAY)
       DIMENSION DRAD1(0:MXLAY),URAD1(0:MXLAY)
-      DIMENSION DRAD2(0:MXLAY),URAD2(0:MXLAY)
-      DIMENSION DRAD3(0:MXLAY),URAD3(0:MXLAY)
-      DIMENSION WTNUM(MXANG)
+
       HVRRTR = '$Revision$'
 
-C *** These weights correspond to angles of 34.9, 65.8, and 74.1
-C     degrees, resp.  They are used when "numerical" Gaussian
-C     quadrature is chosen.
-      DATA WTNUM(3) /0.1084176674/, WTNUM(2) /0.0424353369/
-      DATA WTNUM(1) /0.3491473794/
-
-C *** SECANG is equal to the secant of the first angle.
-      SECANG = 1.219512195
+      DATA SECDIFF /1.66/
+      DATA WTDIFF /0.5/
+      DATA REC_6 /0.166667/
 
       DO 200 LAY = 0, NLAYERS
          URAD1(LAY) = 0.0
-         URAD2(LAY) = 0.0
-         URAD3(LAY) = 0.0
          DRAD1(LAY) = 0.0
-         DRAD2(LAY) = 0.0
-         DRAD3(LAY) = 0.0
          TOTUFLUX(LAY) = 0.0
          TOTDFLUX(LAY) = 0.0
  200  CONTINUE
@@ -102,80 +97,40 @@ C *** Loop over frequency bands.
          ELSEIF (IBAND .EQ. 16) THEN
             CALL TAUGB16
          ENDIF
-        
 C ***    Loop over g-channels.
+
          IG = 1
  1000    CONTINUE
 C ***    Radiative transfer starts here.
+
          RADLD1 = 0.
-         RADLD2 = 0.
-         RADLD3 = 0.
 C ***    Downward radiative transfer.  Due to the simple form taken by
-C        certain equations when the optical depth is either small or large, 
-C        these conditions are tested for.  In this section, the labels 
-C        1, 2, and 3 refer to the three angles.
-         BGLEV = FRACS(NLAYERS,IG) * PLANKLEV(NLAYERS,IBAND)
+C        certain equations when the optical depth is small,
+C        these conditions are tested for.  
          DO 2500 LEV = NLAYERS, 1, -1
             BLAY = PLANKLAY(LEV,IBAND)
             PLFRAC = FRACS(LEV,IG)
-            BGLAY = PLFRAC * BLAY
-            ODEPTH = SECANG * TAUG(LEV,IG)
-            IF (ODEPTH .LE. 1.E-2) THEN
-               ATRANS1(LEV) = ODEPTH
-               ATRANS2(LEV) = ODEPTH + ODEPTH
-               ATRANS3(LEV) = ATRANS2(LEV) + ODEPTH
-               BBU1(LEV) = BGLAY
-               BBU2(LEV) = BGLAY
-               BBU3(LEV) = BGLAY
-               RADLD1 = RADLD1 + (BGLAY-RADLD1)*ATRANS1(LEV)
-               RADLD2 = RADLD2 + (BGLAY-RADLD2)*ATRANS2(LEV)
-               RADLD3 = RADLD3 + (BGLAY-RADLD3)*ATRANS3(LEV)
-               BGLEV = PLFRAC * PLANKLEV(LEV-1,IBAND)
-            ELSEIF (ODEPTH .GE. 10.) THEN
-               ATRANS1(LEV) = 1.
-               ATRANS2(LEV) = 1.
-               ATRANS3(LEV) = 1.
-               DELBGUP = BGLEV - BGLAY
-               BGLEV = PLFRAC * PLANKLEV(LEV-1,IBAND)
-               DELBGDN = BGLEV - BGLAY
-               TAUSFAC = ODEPTH/(5.+ODEPTH)
-               BBU1(LEV) = BGLAY + TAUSFAC * DELBGUP
-               RADLD1 = BGLAY + TAUSFAC * DELBGDN
-               TAUSFAC = ODEPTH/(2.5+ODEPTH)
-               BBU2(LEV) = BGLAY + TAUSFAC * DELBGUP
-               RADLD2 = BGLAY + TAUSFAC * DELBGDN
-               TAUSFAC = ODEPTH/(1.666667+ODEPTH)
-               BBU3(LEV) = BGLAY + TAUSFAC * DELBGUP
-               RADLD3 = BGLAY + TAUSFAC * DELBGDN
+            DPLANKUP = PLANKLEV(LEV,IBAND) - BLAY
+            DPLANKDN = PLANKLEV(LEV-1,IBAND) - BLAY
+            ODEPTH1 = SECDIFF*TAUG(LEV,IG)
+            IF (ODEPTH1 .LE. 0.06) THEN
+               IF (ODEPTH1 .LT. 0.0) ODEPTH1 = 0.0
+               ATRANS1(LEV) = ODEPTH1-0.5*ODEPTH1*ODEPTH1
+               ODEPTH1 = REC_6*ODEPTH1
+               BBD1 =  PLFRAC*(BLAY+DPLANKDN*ODEPTH1)
+               RADLD1 = RADLD1 + (BBD1-RADLD1)*ATRANS1(LEV)
+               BBU1(LEV) =  PLFRAC*(BLAY+DPLANKUP*ODEPTH1)
             ELSE
-               TRANS1 = EXP(-ODEPTH)
+               TBLIND = ODEPTH1/(BPADE+ODEPTH1)
+               ITR1 = TBLINT*TBLIND+0.5
+               TRANS1 = TRANS(ITR1)
                ATRANS1(LEV) = 1. - TRANS1
-               ATRANS2(LEV) = 1. - TRANS1*TRANS1
-               ATRANS3(LEV) = ATRANS1(LEV) + TRANS1*ATRANS2(LEV)
-               DELBGUP = BGLEV - BGLAY
-               BGLEV = PLFRAC * PLANKLEV(LEV-1,IBAND)
-               DELBGDN = BGLEV - BGLAY
-
-C ***          TAUSFAC is needed for the Pade approximation for
-C              the Planck function.
-               TAUSFAC = ODEPTH/(5.+ODEPTH)
-               BBD = BGLAY + TAUSFAC * DELBGDN
-               RADLD1 = RADLD1 + (BBD-RADLD1)*ATRANS1(LEV)
-               BBU1(LEV) = BGLAY + TAUSFAC * DELBGUP
-
-               TAUSFAC = ODEPTH/(2.5+ODEPTH)
-               BBD = BGLAY + TAUSFAC * DELBGDN
-               RADLD2 = RADLD2 + (BBD-RADLD2)*ATRANS2(LEV)
-               BBU2(LEV) = BGLAY + TAUSFAC * DELBGUP
-
-               TAUSFAC = ODEPTH/(1.6666667+ODEPTH)
-               BBD = BGLAY + TAUSFAC * DELBGDN
-               RADLD3 = RADLD3 + (BBD-RADLD3)*ATRANS3(LEV)
-               BBU3(LEV) = BGLAY + TAUSFAC * DELBGUP
-            ENDIF
+               TAUSFAC1 = TF(ITR1)
+               BBD1 = PLFRAC * (BLAY + TAUSFAC1 * DPLANKDN)
+               RADLD1 = RADLD1 + (BBD1-RADLD1)*ATRANS1(LEV)
+               BBU1(LEV) = PLFRAC * (BLAY + TAUSFAC1 * DPLANKUP)
+            ENDIF   
             DRAD1(LEV-1) = DRAD1(LEV-1) + RADLD1
-            DRAD2(LEV-1) = DRAD2(LEV-1) + RADLD2
-            DRAD3(LEV-1) = DRAD3(LEV-1) + RADLD3
  2500    CONTINUE
 
 C ***    Upward radiative transfer.
@@ -185,26 +140,15 @@ C        Add in reflection of surface downward radiance.
          IF (IREFLECT .EQ. 1) THEN
 C           Specular reflection.
             RADLU1 = RAD0 + REFLECT * RADLD1
-            RADLU2 = RAD0 + REFLECT * RADLD2
-            RADLU3 = RAD0 + REFLECT * RADLD3
          ELSE
 C           Lambertian reflection.
-            RAD = 2. * (RADLD1*WTNUM(1) + RADLD2*WTNUM(2) + 
-     &           RADLD3*WTNUM(3))
+            RAD = 2. * (RADLD1*WTDIFF)
             RADLU1 = RAD0 + REFLECT * RAD
-            RADLU2 = RADLU1
-            RADLU3 = RADLU1
          ENDIF
          URAD1(0) = URAD1(0) + RADLU1
-         URAD2(0) = URAD2(0) + RADLU2
-         URAD3(0) = URAD3(0) + RADLU3
          DO 2600 LEV = 1, NLAYERS
             RADLU1 = RADLU1 + (BBU1(LEV)-RADLU1)*ATRANS1(LEV)
-            RADLU2 = RADLU2 + (BBU2(LEV)-RADLU2)*ATRANS2(LEV)
-            RADLU3 = RADLU3 + (BBU3(LEV)-RADLU3)*ATRANS3(LEV)
             URAD1(LEV) = URAD1(LEV) + RADLU1
-            URAD2(LEV) = URAD2(LEV) + RADLU2
-            URAD3(LEV) = URAD3(LEV) + RADLU3
  2600    CONTINUE
  4000    CONTINUE
          IG = IG + 1
@@ -213,21 +157,14 @@ C           Lambertian reflection.
 C ***    Process longwave output from band.
 C ***    Calculate upward, downward, and net flux.
          DO 5000 LEV = NLAYERS, 0, -1
-            UFLUX(LEV) = URAD1(LEV)*WTNUM(1) + URAD2(LEV)*WTNUM(2) + 
-     &           URAD3(LEV)*WTNUM(3) 
-            DFLUX(LEV) = DRAD1(LEV)*WTNUM(1) + DRAD2(LEV)*WTNUM(2) + 
-     &           DRAD3(LEV)*WTNUM(3)
+            UFLUX(LEV) = URAD1(LEV)*WTDIFF
+            DFLUX(LEV) = DRAD1(LEV)*WTDIFF
             URAD1(LEV) = 0.0
-            URAD2(LEV) = 0.0
-            URAD3(LEV) = 0.0
             DRAD1(LEV) = 0.0
-            DRAD2(LEV) = 0.0
-            DRAD3(LEV) = 0.0
             TOTUFLUX(LEV) = TOTUFLUX(LEV) + UFLUX(LEV) * DELWAVE(IBAND)
             TOTDFLUX(LEV) = TOTDFLUX(LEV) + DFLUX(LEV) * DELWAVE(IBAND)
  5000    CONTINUE
  6000 CONTINUE
-
       TOTUFLUX(0) = TOTUFLUX(0) * FLUXFAC
       TOTDFLUX(0) = TOTDFLUX(0) * FLUXFAC
       FNET(0) = TOTUFLUX(0) - TOTDFLUX(0)
@@ -236,7 +173,6 @@ C ***    Calculate upward, downward, and net flux.
          TOTDFLUX(LEV) = TOTDFLUX(LEV) * FLUXFAC
          FNET(LEV) = TOTUFLUX(LEV) - TOTDFLUX(LEV)
          L = LEV - 1
-
 C        Calculate Heating Rates.
          HTR(L)=HEATFAC*(FNET(L)-FNET(LEV))/(PZ(L)-PZ(LEV)) 
  7000 CONTINUE
@@ -245,4 +181,6 @@ C        Calculate Heating Rates.
  9000 CONTINUE
 
       RETURN
+
       END   
+
