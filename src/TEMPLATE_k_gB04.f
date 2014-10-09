8673931ad184008c177eb135e9ba5b7d236e2785
jdelamer <jdelamer>
2014-10-09 00:59:38 +0000

    Errant comma in declaration statement.

--- src/TEMPLATE_k_gB04.f
+++ src/TEMPLATE_k_gB04.f
@@ -16,7 +16,7 @@ C  --------------------------------------------------------------------------
       PARAMETER (MG=16)
 
       REAL KA(9,5,13,MG), KB(5,5,13:59,MG)
-      DIMENSION SELFREF(10,MG), FORREF(4,MG), 
+      DIMENSION SELFREF(10,MG), FORREF(4,MG)
       DIMENSION FRACREFA(MG,9), FRACREFB(MG,5)
 
       COMMON /CVRSN4/ HNAMKG4,HVRKG4
