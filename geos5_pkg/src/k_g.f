bf3992056d99848b0c979ecd62c22f772320664e
kcadyper <kcadyper>
2015-11-17 21:45:54 +0000

    Tagged code delivered to KAUST (original and updates); thsi version delivered to GEOS5 group.

---
+++ geos5_pkg/src/k_g.f
@@ -0,0 +1,104 @@
+C     path:      $Source$
+C     author:    $Author: jdelamer $
+C     revision:  $Revision: 11468 $
+C     created:   $Date: 2004-07-29 12:49:51 -0400 (Thu, 29 Jul 2004) $
+****************************************************************************
+C
+C  --------------------------------------------------------------------------
+C |                                                                          |
+C |  Copyright 2002, 2003, Atmospheric & Environmental Research, Inc. (AER). |
+C |  This software may be used, copied, or redistributed as long as it is    |
+C |  not sold and this copyright notice is reproduced on each copy made.     |
+C |  This model is provided as is without any express or implied warranties. |
+C |                       (http://www.rtweb.aer.com/)                        |
+C |                                                                          |
+C  --------------------------------------------------------------------------
+
+      BLOCK DATA KGB1
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB01.f'
+      END
+
+      BLOCK DATA KGB2
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB02.f'
+      END
+
+      BLOCK DATA KGB3
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB03.f'
+      END
+
+      BLOCK DATA KGB4
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB04.f'
+      END  
+
+      BLOCK DATA KGB5
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB05.f'
+      END
+
+      BLOCK DATA KGB6
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB06.f'
+      END
+
+      BLOCK DATA KGB7
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB07.f'
+      END
+
+      BLOCK DATA KGB8
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB08.f'
+      END
+
+      BLOCK DATA KGB9
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB09.f'
+      END
+
+      BLOCK DATA KGB10
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB10.f'
+      END
+
+      BLOCK DATA KGB11
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB11.f'
+      END
+
+      BLOCK DATA KGB12
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB12.f'
+      END
+
+      BLOCK DATA KGB13
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB13.f'
+      END
+
+      BLOCK DATA KGB14
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB14.f'
+      END
+
+      BLOCK DATA KGB15
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB15.f'
+      END
+
+      BLOCK DATA KGB16
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB16.f'
+      END
+
+      BLOCK DATA KGB17
+      PARAMETER (NBANDs = 17)
+      INCLUDE 'k_gB17.f'
+      END
+
+      
+****************************************************************************
+
